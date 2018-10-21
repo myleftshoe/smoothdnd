ec9e602d4a235b013d4be982a5095464c8f866da
Paul <paulesammut@gmail.com>
2018-10-21 19:08:41 +1100

    Updates

---
+++ precache-manifest.f8d01aedf2c275dcfdb11d5eebb5fcd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smoothdnd/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "59ef93980a7f651c6420",
+    "url": "/smoothdnd/static/js/runtime~main.59ef9398.js"
+  },
+  {
+    "revision": "64af9f882a68e5fd6b8b",
+    "url": "/smoothdnd/static/js/main.64af9f88.chunk.js"
+  },
+  {
+    "revision": "1ed744f0fd935e06b7d2",
+    "url": "/smoothdnd/static/js/1.1ed744f0.chunk.js"
+  },
+  {
+    "revision": "64af9f882a68e5fd6b8b",
+    "url": "/smoothdnd/static/css/main.69f0c4e0.chunk.css"
+  },
+  {
+    "revision": "e0751b5d59640b243132e0b91fe01040",
+    "url": "/smoothdnd/index.html"
+  }
+];
